e26669346201020135466fb616b28c20ba1bed47
01-Meyitzade-01 <83854378+01-Meyitzade-01@users.noreply.github.com>
2022-04-05 13:15:59 +0300

    Update config.py

--- eduu/config.py
+++ eduu/config.py
@@ -5,7 +5,7 @@ import os
 
 
 def get_user_list(config, key):
-    with open("{}/EduuRoBot/{}".format(os.getcwd(), config), "r") as json_file:
+    with open("{}/EduuRobot/{}".format(os.getcwd(), config), "r") as json_file:
         return json.load(json_file)[key]
 
 
